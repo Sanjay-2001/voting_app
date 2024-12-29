6d54e6715f8851d001ead8dc825d1b708035ed21
SanjayDutta <sanjaydutta976@gmail.com>
2024-12-29 18:53:49 +0530

    candidate route

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
 const userRoutes = require("./routes/userRoutes");
-// const candidateRoutes = require("./routes/candidateRoutes");
+const candidateRoutes = require("./routes/candidateRoutes");
 
 app.use("/user", userRoutes);
-// app.use("/candidate", candidateRoutes);
+app.use("/candidate", candidateRoutes);
 
 app.listen(PORT, () => {
   console.log("listening on port 3000");
