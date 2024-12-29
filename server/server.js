8872b8744996cec5141bcb4a56150ea0c0654f80
SanjayDutta <sanjaydutta976@gmail.com>
2024-12-29 12:20:40 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+// require("dotenv").config();
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Listening on port 3000");
+});
